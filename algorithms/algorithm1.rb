89f9b5430dee617f24ea85e97a293021418a5567
Hammond Soares <hammond.soares@gmail.com>
2019-03-10 10:36:37 -0400

    Refactoring for algo 1

--- algorithms/algorithm1.rb
+++ algorithms/algorithm1.rb
@@ -1,22 +1,40 @@
 require_relative '../helpers/Point.rb'
 
-def get_point
-  print "Insert coordenate x: "
-  x1 = gets.chomp()
-  print "Insert coordenate y: "
-  y1 = gets.chomp()
+class Algorithm1
 
-  Point.new(x1, y1)
+  attr_accessor :point1, :point2
+
+  def get_point
+    print "Insert coordenate x: "
+    x1 = gets.chomp()
+    print "Insert coordenate y: "
+    y1 = gets.chomp()
+
+    Point.new(x1, y1)
+  end
+
+  def calculate_distance
+    @point1.distance_to(@point2)
+  end
+
+  def initialize
+    # Criates 1st point
+    @point1 = get_point
+    # Creates 2nd point
+    @point2 = get_point
+
+    # Print the result
+    puts "Result is :"
+    puts calculate_distance.to_s
+  end
+
+  private
+
+  def create_point
+    point = get_point
+    puts "Point1 = " + p1.to_s
+    point
+  end
 end
 
-# Criates 1st point
-p1 = get_point
-puts "Point1 = " + p1.to_s
-
-# Creates 2nd point
-p2 = get_point
-puts "Point2 = " + p2.to_s
-
-# Print the result
-puts "Result is :"
-puts p1.distance_to(p2).to_s
+Algorithm1.new()
